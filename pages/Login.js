f1701691e4f0f5aaea2511e45a33ec8e46a00237
Alec DiFederico <alecdifederico@gmail.com>
2020-02-23 12:46:13 -0500

    fully implemented login process

--- pages/Login.js
+++ pages/Login.js
@@ -1,87 +1,98 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
-import { Button, Input } from 'react-native-elements';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert, ActivityIndicator } from 'react-native';
+import { Button, Input, Overlay } from 'react-native-elements';
 
 import Container from '../components/Container'
 import { db, auth } from '../Firebase';
 
 
-const users = db.collection('users');
+export default function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-export default class Login extends Component {
-  state = {
-    selectedPlayer: '',
-    players: [],
-    email: '',
-    password: ''
-  };
+  useEffect(() => {
+    auth.onAuthStateChanged(async user => {
+      if (user) {
+        const currentUser = await db
+          .collection('users')
+          .doc(user.uid)
+          .get()
 
-  componentDidMount() {
-    users.get().then(snapshot => {
-      let players = [];
-      snapshot.forEach(doc => {
-        let player = doc.data().firstName + ' ' + doc.data().lastName;
-        players.push(player);
-      });
-      this.setState({players: players});
-    });
-  }
-
-  render() {
-    return (
-      <Container>
-        <Text style={styles.welcome}>WBL Stats Sheet</Text>
-        {/* <Picker 
-          style={styles.picker} 
-          selectedValue={this.state.selectedPlayer}
-          onValueChange={value => this.setState({selectedPlayer: value})}
-        >
-          {this.state.players.map((value, index) => {
-            return <Picker.Item key={index} label={value.toString()} value={value} />
-          })}
-        </Picker> */}
-
-
-        <View style={{ width: '80%', marginTop: 30}}> 
-          <Input
-            label='Email Address'
-            placeholder='email@address.com'
-            onChangeText={email => this.setState({ email })}
-            leftIcon={{ type: 'font-awesome', name: 'envelope', size: 20 }}
-            keyboardType='email-address'
-            leftIconContainerStyle={{ marginRight: 15, marginLeft: 0, paddingLeft: 0}}
-          />
-        </View>
-
-        <View style={{ width: '80%', marginTop: 30}}> 
-          <Input
-            label='Password'
-            placeholder='password'
-            onChangeText={password => this.setState({ password })}
-            leftIcon={{ type: 'font-awesome', name: 'lock', size: 24 }}
-            secureTextEntry={true}
-            leftIconContainerStyle={{ marginRight: 15, marginLeft: 0, paddingLeft: 0}}
-          />
-        </View>
+        setIsLoading(false);
         
-        <View style={{width:'80%', marginTop: 30}}>
-          <Button
-            buttonStyle={{ height: 50 }}
-            titleStyle={{ fontWeight: 'bold'}}
-            title="Login"
-            onPress={() => {
-              let values = {
-                player: this.state.selectedPlayer, 
-                opponents: this.state.players.filter(p => p != this.state.selectedPlayer)
-              };
-              this.props.navigation.navigate('Form', values);
-            }}
-          />
-        </View>
-        <Button type="clear" title="No account yet? Sign up" onPress={() => this.props.navigation.navigate('SignUp')} />
-      </Container>
-    );
+        if (currentUser != null) {
+          props.navigation.navigate('Form', currentUser.data());
+        } 
+      }
+    })
+  }, []);
+
+  async function handleLogin() {
+    try {
+      const response = await auth.signInWithEmailAndPassword(email, password);
+
+      if (response.user.uid) {
+        const user = await db
+          .collection('users')
+          .doc(response.user.uid)
+          .get()
+  
+        props.navigation.navigate('Form', user.data());
+      }
+    } catch (e) {
+      Alert.alert(e.toString());
+    }
   }
+
+  return (
+    <Container>
+      <Text style={styles.welcome}>WBL Stats Sheet</Text>
+      
+      <View style={{ width: '80%', marginTop: 30}}> 
+        <Input
+          label='Email Address'
+          placeholder='email@address.com'
+          onChangeText={email => setEmail(email)}
+          leftIcon={{ type: 'font-awesome', name: 'envelope', size: 20 }}
+          keyboardType='email-address'
+          leftIconContainerStyle={{ marginRight: 15, marginLeft: 0, paddingLeft: 0}}
+        />
+      </View>
+
+      <View style={{ width: '80%', marginTop: 30}}> 
+        <Input
+          label='Password'
+          placeholder='password'
+          onChangeText={password => setPassword(password)}
+          leftIcon={{ type: 'font-awesome', name: 'lock', size: 24 }}
+          secureTextEntry={true}
+          leftIconContainerStyle={{ marginRight: 15, marginLeft: 0, paddingLeft: 0}}
+        />
+      </View>
+      
+      <View style={{width:'80%', marginTop: 30}}>
+        <Button
+          buttonStyle={{ height: 50 }}
+          titleStyle={{ fontWeight: 'bold'}}
+          title="Login"
+          onPress={() => handleLogin()}
+        />
+      </View>
+      <Button type="clear" title="No account yet? Sign up" onPress={() => props.navigation.navigate('SignUp')} />
+
+      <Overlay 
+          isVisible={isLoading}
+          width="100%"
+          height="100%"
+          overlayBackgroundColor="rgba(0,0,0, 1)"
+        >
+          <View style={{ flex: 1, justifyContent: 'center'}}>
+            <ActivityIndicator size="large" color="#ffffff" />
+          </View>
+        </Overlay>
+    </Container>
+  );
 }
 
 const styles = StyleSheet.create({
