88f232696ccb67d7b7c99e9f40a90fed8debc8ad
alec-difederico <adifederico@gocatalant.com>
2020-06-07 16:22:00 -0400

    removed a comment

--- actions/currentUserActions.js
+++ actions/currentUserActions.js
@@ -9,23 +9,6 @@ export const loginCurrentUser = currentUser => ({
     currentUser
 })
 
-// export const logoutCurrentUser = () => ({
-//     type: LOGOUT_USER
-// });
-
-// function logoutCurrentUser() {
-//     // We can invert control here by returning a function - the "thunk".
-//     // When this function is passed to `dispatch`, the thunk middleware will intercept it,
-//     // and call it with `dispatch` and `getState` as arguments.
-//     // This gives the thunk function the ability to run some logic, and still interact with the store.
-//     return function(dispatch) {
-//         return fetchSecretSauce().then(
-//         (sauce) => dispatch(makeASandwich(forPerson, sauce)),
-//         (error) => dispatch(apologize('The Sandwich Shop', forPerson, error)),
-//         );
-//     };
-// }
-
 export const logoutCurrentUser = () => {
   return dispatch => {
       return new Promise((resolve, reject) => {
